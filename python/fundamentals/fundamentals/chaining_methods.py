10a4d8b1e62493f0bc72e55ec616909792ddf92e
jaerrib <jaerrib@tutanota.com>
2023-03-13 12:19:25 -0400

    Add chaining_methods assignment

---
+++ python/fundamentals/fundamentals/chaining_methods.py
@@ -0,0 +1,53 @@
+class User():
+
+    def __init__(self, first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_reward_member = False
+        self.gold_card_points = 0
+
+    def display_info(self):
+        # Prints all of the users' details on separate lines
+        print("First name:", self.first_name)
+        print("Last name:", self.last_name)
+        print("Email:", self.email)
+        print("Age:", self.age)
+        print("Is reward member:", self.is_reward_member)
+        print("Gold card points:", self.gold_card_points)
+        return self
+
+    def enroll(self):
+        """
+        Changes the user's member status to True and set their gold card points to 200
+        Check if they are a member already, and if they are, print "User already a
+        member." and return False, otherwise return True.
+        """
+        if self.is_reward_member == True:
+            print("User already a member")
+            return self
+        else:
+            self.is_reward_member = True
+            self.gold_card_points = 200
+            return self
+
+    def spend_gold_points(self, amount):
+        """
+        Decreases the user's points by the amount specified
+        Checks to be sure they have enough points to spend that amount
+        and handle appropriately.
+        """
+        if amount <= self.gold_card_points:
+            self.gold_card_points -= amount
+            # print(self.gold_card_points,"gold card points left")
+        else:
+            print("Not enough gold points")
+        return self
+
+user1 = User("John", "Doe", "jdoe@domain.com", 18)
+user2 = User("Jane", "Doe", "jane_doe@domain.com", 21)
+user3 = User("Linus", "Torvalds", "linuxguru@domain.com", 53)
+
+user1.display_info().enroll().spend_gold_points(50).display_info()
+user2.enroll().spend_gold_points(80).display_info()
