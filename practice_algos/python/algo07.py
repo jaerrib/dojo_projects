6c68ba492738a3a59af3acc0e5172d5d9764520c
jaerrib <jaerrib@tutanota.com>
2023-03-21 19:11:40 -0400

    Fix merge issue problems

--- practice_algos/python/algo07.py
+++ practice_algos/python/algo07.py
@@ -1,7 +1,4 @@
 # 7. Array odd - Write a function that would return an array of all the odd
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 # numbers between 1 to 50. (ex. [1,3,5, .... , 47,49]). Hint: Use 'append' method
 
 def arr_odd():
@@ -12,20 +9,3 @@ def arr_odd():
     return arr
 
 print(arr_odd())
-=======
->>>>>>> Stashed changes
-# numbers between 1 to 50. (ex. [1,3,5, .... , 47,49]).
-
-def array_odd():
-    temp_arr = []
-    for i in range(1, 50):
-        if i % 2 != 0:
-            temp_arr.append(i)
-    return temp_arr
-
-<<<<<<< Updated upstream
-print(array_odd())
-=======
-print(array_odd())
->>>>>>> main
->>>>>>> Stashed changes
