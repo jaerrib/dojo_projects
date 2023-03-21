f868f3cc903d5b716d915453d059101117f89cd0
jaerrib <jaerrib@tutanota.com>
2023-03-21 19:04:39 -0400

    Add algo06 solution

---
+++ practice_algos/python/algo06.py
@@ -0,0 +1,12 @@
+# 6. Find average - Given an array with multiple values, write a function
+# that returns the average of the values in the array. (e.g. for
+# [1,3,5,7,20] average is 7.2)
+
+def find_avg(arr):
+    sum = 0
+    for num in arr:
+        sum += num
+    return sum / len(arr)
+
+
+print(find_avg([1, 3, 5, 7, 20]))
