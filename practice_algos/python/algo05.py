981d9671ef6c6c1ab53d82cc0b37f27bcd4545ec
jaerrib <jaerrib@tutanota.com>
2023-03-21 19:02:46 -0400

    Add algo05 solution

---
+++ practice_algos/python/algo05.py
@@ -0,0 +1,11 @@
+# 5. Find max - Given an array with multiple values, write a function that
+# returns the maximum number in the array. (e.g. for [-3,3,5,7] max is 7)
+
+def find_max(arr):
+    max = arr[0]
+    for num in arr:
+        if num > max:
+            max = num
+    return max
+
+print(find_max([-3,3,5,7]))
