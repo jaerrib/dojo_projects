596354ba3a4793a907c040c8d055c5bf177a13d4
jaerrib <jaerrib@tutanota.com>
2023-03-21 21:17:33 -0400

    Add basketball_dictionaries assignment - Complete challeneges 1 and 2

---
+++ python/fundamentals/oop/basketball_dictionaries.py
@@ -0,0 +1,73 @@
+players = [
+    {
+        "name": "Kevin Durant",
+        "age": 34,
+        "position": "small forward",
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum",
+        "age": 24,
+        "position": "small forward",
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving",
+        "age": 32, "position": "Point Guard",
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard",
+        "age": 33, "position": "Point Guard",
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid",
+        "age": 32, "position": "Power Foward",
+        "team": "Philidelphia 76ers"
+    },
+    {
+        "name": "",
+        "age": 16,
+        "position": "P",
+        "team": "en"
+    }
+]
+
+
+kevin = {
+    "name": "Kevin Durant",
+    "age": 34,
+    "position": "small forward",
+    "team": "Brooklyn Nets"
+}
+jason = {
+    "name": "Jason Tatum",
+    "age": 24,
+    "position": "small forward",
+    "team": "Boston Celtics"
+}
+kyrie = {
+    "name": "Kyrie Irving",
+    "age": 32, "position": "Point Guard",
+    "team": "Brooklyn Nets"
+}
+
+# Create your Player instances here!
+# player_jason = ???
+
+
+class Player:
+    def __init__(self, player_info):
+        self.name = player_info["name"]
+        self.age = player_info["age"]
+        self.position = player_info["position"]
+        self.team = player_info["team"]
+
+
+player_kevin = Player(kevin)
+player_jason = Player(jason)
+player_kyrie = Player(kyrie)
+
+
+print(player_kevin.name)
