7501200bfa7207a7ef96b90848dafb5ddaeed7a0
jaerrib <jaerrib@tutanota.com>
2023-04-04 19:51:45 -0400

    Add friendships assignment (partially complete)

---
+++ python/mysql/queries/Beers_John_Friendships.sql
@@ -0,0 +1,56 @@
+-- INSERT INTO users (first_name, last_name)
+-- VALUES ('Amy', 'Giver');
+
+-- INSERT INTO users (first_name, last_name)
+-- VALUES ('Eli', 'Byers');
+
+-- INSERT INTO users (first_name, last_name)
+-- VALUES ('Marky', 'Mark');
+
+-- INSERT INTO users (first_name, last_name)
+-- VALUES ('Big', 'Bird');
+
+-- INSERT INTO users (first_name, last_name)
+-- VALUES ('Kermit', 'The Frog');
+
+-- INSERT INTO users (first_name, last_name)
+-- VALUES ('Oscar', 'The Grouch');
+
+-- INSERT INTO friendships (user_id, friend_id)
+-- VALUES (1, 2);
+
+-- INSERT INTO friendships (user_id, friend_id)
+-- VALUES (1, 4);
+
+-- INSERT INTO friendships (user_id, friend_id)
+-- VALUES (1, 6);
+
+-- INSERT INTO friendships (user_id, friend_id)
+-- VALUES (2, 1);
+
+-- INSERT INTO friendships (user_id, friend_id)
+-- VALUES (2, 3);
+
+-- INSERT INTO friendships (user_id, friend_id)
+-- VALUES (2, 5);
+
+-- INSERT INTO friendships (user_id, friend_id)
+-- VALUES (3, 2);
+
+-- INSERT INTO friendships (user_id, friend_id)
+-- VALUES (3, 5);
+
+-- INSERT INTO friendships (user_id, friend_id)
+-- VALUES (4, 3);
+
+-- INSERT INTO friendships (user_id, friend_id)
+-- VALUES (5, 1);
+
+-- INSERT INTO friendships (user_id, friend_id)
+-- VALUES (5, 6);
+
+-- INSERT INTO friendships (user_id, friend_id)
+-- VALUES (6, 2);
+
+-- INSERT INTO friendships (user_id, friend_id)
+-- VALUES (6, 3);
