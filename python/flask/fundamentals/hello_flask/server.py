101e9570f15732e0a2f69cd628937f5d3b1e2ad6
jaerrib <jaerrib@tutanota.com>
2023-04-10 12:38:27 -0400

    Add list passing example

--- python/flask/fundamentals/hello_flask/server.py
+++ python/flask/fundamentals/hello_flask/server.py
@@ -17,5 +17,17 @@ def hello(username, num):
     return render_template("hello.html", username=username, num=num)
 
 
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name': 'Michael', 'age': 35},
+        {'name': 'John', 'age': 30},
+        {'name': 'Mark', 'age': 25},
+        {'name': 'KB', 'age': 27}
+    ]
+    return render_template("lists.html", random_numbers=[3, 1, 5], students=student_info)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
