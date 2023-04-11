5bebb6aa92b22264b6aeb78beaffe9bb39a32763
jaerrib <jaerrib@tutanota.com>
2023-04-11 12:33:12 -0400

    Finish playground assignment

--- python/flask/fundamentals/playground/server.py
+++ python/flask/fundamentals/playground/server.py
@@ -3,8 +3,16 @@ app = Flask(__name__)
 
 
 @app.route('/play')
-def play():
-    return render_template("play.html", num=3)
+def play(num, color):
+    return render_template("play.html", num=3, color=blue)
+
+@app.route('/play/<int:num>')
+def repeat(num, color):
+    return render_template("play.html", num=num, color=blue)
+
+@app.route('/play/<int:num>/<string:color>')
+def repeat_color(num, color):
+    return render_template("play.html", num=num, color=color)
 
 
 if __name__ == "__main__":
