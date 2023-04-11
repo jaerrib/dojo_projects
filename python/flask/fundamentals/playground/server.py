9e4d7973059da672c0651779d7c5db5f8976a725
jaerrib <jaerrib@tutanota.com>
2023-04-11 18:27:25 -0400

    Fix variable bug
    - Bug present on secondary system

--- python/flask/fundamentals/playground/server.py
+++ python/flask/fundamentals/playground/server.py
@@ -3,12 +3,14 @@ app = Flask(__name__)
 
 
 @app.route('/play')
-def play(num, color):
-    return render_template("play.html", num=3, color=blue)
+def play():
+    return render_template("play.html", num=3, color="blue")
+
 
 @app.route('/play/<int:num>')
-def repeat(num, color):
-    return render_template("play.html", num=num, color=blue)
+def repeat(num):
+    return render_template("play.html", num=num, color="blue")
+
 
 @app.route('/play/<int:num>/<string:color>')
 def repeat_color(num, color):
