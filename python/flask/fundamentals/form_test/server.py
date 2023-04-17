1b8688ce19694fe8d593a5b2a7b4f89ca7ceb06d
jaerrib <jaerrib@tutanota.com>
2023-04-17 19:19:21 -0400

    Add session to form_test
    - Fix index.html template location
    - Add redirect and session variable

--- python/flask/fundamentals/form_test/server.py
+++ python/flask/fundamentals/form_test/server.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+# set a secret key for security purposes
+app.secret_key = 'keep it secret, keep it safe'
 
 # our index route will handle rendering our form
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -10,10 +14,17 @@ def index():
 @app.route('/users', methods=['POST'])
 def create_user():
     print("Got Post Info")
-    print(request.form)
-    # Never render a template on a POST request.
-    # Instead we will redirect to our index route.
-    return redirect('/')
+    # Here we add two properties to session to store the name and email
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect('/show')
+
+# adding this method
+
+
+@app.route('/show')
+def show_user():
+    return render_template('show.html')
 
 
 if __name__ == "__main__":
