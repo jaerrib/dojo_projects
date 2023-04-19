3c826e6cde6d9b54a2956ca4e1f2e66f29d04abf
jaerrib <jaerrib@tutanota.com>
2023-04-19 12:24:27 -0400

    add counter assignment (with ninja bonuses)

---
+++ python/flask/fundamentals/counter/server.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+
+@app.route('/')
+def index():
+    if not "visits" in session:
+        session["visits"] = 0
+    session["visits"] += 1
+    return render_template("index.html")
+
+
+@app.route('/increase_count')
+def increase_count():
+    session["visits"] += 1
+    return redirect('/')
+
+
+@app.route('/destroy_session')
+def destroy_session():
+    session.clear()
+    return redirect('/')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+# @app.route('/add2')
+# def increase_count(amount):
+#     session["visits"] += int(amount)
+#     return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
