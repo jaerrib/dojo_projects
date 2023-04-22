154613d935b79e0e16cbe6bd84757b0eb0f690a3
jaerrib <jaerrib@tutanota.com>
2023-04-22 06:31:22 -0400

    Add money processing functionality

--- python/flask/fundamentals/ninja_gold/server.py
+++ python/flask/fundamentals/ninja_gold/server.py
@@ -1,23 +1,30 @@
 from flask import Flask, render_template, request, redirect, session
+from random import randint
 app = Flask(__name__)
 app.secret_key = "this is a secret"
 
 @app.route("/")
 def index():
+    if not "gold" in session:
+        session["gold"] = 0
     return render_template("index.html")
 
 @app.route("/process_money", methods=["POST"])
 def process():
     if request.form["building"] == "farm":
         print("Farm")
+        session["gold"] += randint(10, 20)
         return redirect("/")
     elif request.form["building"]  == "cave":
         print("Cave")
+        session["gold"] += randint(5, 10)
         return redirect("/")
     elif request.form["building"]  == "house":
         print("House")
+        session["gold"] += randint(2, 5)
         return redirect("/")
     elif request.form["building"]  == "casino":
+        session["gold"] =+ randint(-50, 50)
         print("Casino")
         return redirect("/")
 
