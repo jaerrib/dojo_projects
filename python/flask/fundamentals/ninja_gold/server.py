a3da137edf9f15b69c9e689433a430bd8990a1f3
jaerrib <jaerrib@tutanota.com>
2023-04-23 10:15:38 -0400

    Add activity logging and reset functionality

--- python/flask/fundamentals/ninja_gold/server.py
+++ python/flask/fundamentals/ninja_gold/server.py
@@ -1,32 +1,56 @@
 from flask import Flask, render_template, request, redirect, session
 from random import randint
+from datetime import date, time, datetime
 app = Flask(__name__)
 app.secret_key = "this is a secret"
+
 
 @app.route("/")
 def index():
     if not "gold" in session:
         session["gold"] = 0
+    if not "activities" in session:
+        session["activities"] = []
     return render_template("index.html")
+
 
 @app.route("/process_money", methods=["POST"])
 def process():
+    selection = request.form["building"]
+    event_time = f" ({datetime.now().strftime('%Y/%m/%d %l:%M %p')})"
+    
+    # match selection:
+    #     case "farm":
+    #         something here
+    #     case "cave":
+    #         something here
+    #     case "house":
+    #         something here
+    #     case "casino":
+    #         something here
+
     if request.form["building"] == "farm":
-        print("Farm")
-        session["gold"] += randint(10, 20)
-        return redirect("/")
-    elif request.form["building"]  == "cave":
-        print("Cave")
-        session["gold"] += randint(5, 10)
-        return redirect("/")
-    elif request.form["building"]  == "house":
-        print("House")
-        session["gold"] += randint(2, 5)
-        return redirect("/")
-    elif request.form["building"]  == "casino":
-        session["gold"] =+ randint(-50, 50)
-        print("Casino")
-        return redirect("/")
+        gold = randint(10, 20)
+    elif request.form["building"] == "cave":
+        gold = randint(5, 10)
+    elif request.form["building"] == "house":
+        gold = randint(2, 5)
+    elif request.form["building"] == "casino":
+        gold = randint(-50, 50)
+    if gold >= 0:
+        message = f"Earned {gold} gold from the {selection}!" + event_time
+    if gold >= 0 and selection == "casino":
+        message = f"Entered a casino and earned {gold} gold!" + event_time
+    if gold < 0 and selection == "casino":
+        message = f"Entered a casino and lost {gold} gold...ouch!" + event_time
+    session["activities"].append(message)
+    session["gold"] += gold
+    return redirect("/")
+
+@app.route("/reset")
+def reset():
+    session.clear()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
