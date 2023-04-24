1c8b094cd257210e33aa868724bc6c7ba392e22a
jaerrib <jaerrib@tutanota.com>
2023-04-24 12:50:58 -0400

    Add game over conditions

--- python/flask/fundamentals/ninja_gold/server.py
+++ python/flask/fundamentals/ninja_gold/server.py
@@ -13,6 +13,8 @@ def index():
         session["activities"] = []
     if not "moves" in session:
         session["moves"] = 0
+    if not "game_over" in session:
+        session["game_over"] = False
     return render_template("index.html")
 
 
@@ -20,16 +22,6 @@ def index():
 def process():
     selection = request.form["building"]
     event_time = f" ({datetime.now().strftime('%Y/%m/%d %l:%M %p')})"
-    
-    # match selection:
-    #     case "farm":
-    #         something here
-    #     case "cave":
-    #         something here
-    #     case "house":
-    #         something here
-    #     case "casino":
-    #         something here
 
     if request.form["building"] == "farm":
         gold = randint(10, 20)
@@ -42,16 +34,24 @@ def process():
     if gold >= 0:
         message = f"Earned {gold} gold from the {selection}!"
     if gold < 0 and selection == "casino":
-        message = f"Entered a casino and lost {gold} gold...ouch!"
+        message = f"Entered a casino and lost {-(gold)} gold...ouch!"
     session["activities"].append(message + event_time)
     session["gold"] += gold
     session["moves"] += 1
+    if session["gold"] > 250:
+        session["activities"].append("You win!")
+        session["game_over"] = True
+    elif session["moves"] >= 15:
+        session["game_over"] = True
+        session["activities"].append("You lose!")
     return redirect("/")
+
 
 @app.route("/reset")
 def reset():
     session.clear()
     return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
