abfe0627b3b53cb435cd25cbb0d7157f6fa6b67b
jaerrib <jaerrib@tutanota.com>
2023-05-01 12:26:33 -0400

    Add form entry to create database items

--- python/flask_mysql/crud/first_flask_mysql/friend.py
+++ python/flask_mysql/crud/first_flask_mysql/friend.py
@@ -24,3 +24,10 @@ class Friend:
         for friend in results:
             friends.append(cls(friend))
         return friends
+
+    # class method to save our friend to the database
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO friends ( first_name , last_name , occupation , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(occ)s , NOW() , NOW() );"
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('friends').query_db(query, data)
--- python/flask_mysql/crud/first_flask_mysql/server.py
+++ python/flask_mysql/crud/first_flask_mysql/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 # import the class from friend.py
 from friend import Friend
 app = Flask(__name__)
@@ -9,6 +9,21 @@ def index():
     friends = Friend.get_all()
     print(friends)
     return render_template("index.html", all_friends=friends)
+
+
+@app.route('/create_friend', methods=["POST"])
+def create_friend():
+    # First we make a data dictionary from our request.form coming from our template.
+    # The keys in data need to line up exactly with the variables in our query string.
+    data = {
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "occ": request.form["occ"]
+    }
+    # We pass the data dictionary into the save method from the Friend class.
+    Friend.save(data)
+    # Don't forget to redirect after saving to the database.
+    return redirect('/')
 
 
 if __name__ == "__main__":
