02590341085e5a70a54bb85fc87dfbf9e34a0171
jaerrib <jaerrib@tutanota.com>
2023-05-18 12:49:29 -0400

    Add ninja array to Dojo model

--- python/flask_mysql/crud/dojos_and_ninjas/dojo_app/models/model_dojo.py
+++ python/flask_mysql/crud/dojos_and_ninjas/dojo_app/models/model_dojo.py
@@ -11,6 +11,7 @@ class Dojo:
         self.name = data['name']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+        self.ninjas = []
 
     @classmethod
     def get_all(cls):
@@ -31,10 +32,12 @@ class Dojo:
 
     @classmethod
     def get_dojo_with_ninjas(cls, data):
-        query = "SELECT * FROM dojos JOIN ninjas ON dojos.id = ninjas.dojo_id WHERE dojos.id = %(data)s;"
+        query = """
+        SELECT * FROM dojos LEFT JOIN ninjas ON dojos.id = ninjas.dojo_id WHERE dojos.id = %(data)s;
+        """
         results = connectToMySQL(cls.DB).query_db(query, data)
-        print(query, data, results)
-        dojo = cls(results[0])
+        print(results)
+        dojo = cls( results[0] )
 
         for row_from_db in results:
             ninja_data = {
