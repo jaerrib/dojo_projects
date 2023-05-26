ff0e120358ea5123f4e89b3e657ea6222b40bd48
jaerrib <jaerrib@tutanota.com>
2023-05-25 20:05:06 -0400

    Add order creation

--- python/flask_mysql/crud/cookie_orders/cookie_app/controllers/controller_orders.py
+++ python/flask_mysql/crud/cookie_orders/cookie_app/controllers/controller_orders.py
@@ -1,6 +1,6 @@
 from cookie_app import app
-from flask import render_template, redirect, request
-# from cookie_app.models.model_order import Order
+from flask import render_template, redirect, request, session
+from cookie_app.models.model_order import Order
 
 @app.route('/')
 def index():
@@ -8,4 +8,28 @@ def index():
 
 @app.route('/cookies')
 def cookies():
-    return render_template('cookies.html')
+    orders = Order.get_all()
+    return render_template('cookies.html', orders=orders)
+
+@app.route('/cookies/new')
+def new():
+    return render_template('new.html')
+
+@app.route('/cookies/create', methods=['POST'])
+def create():
+    data = {
+        'customer_name': request.form['customer_name'],
+        'cookie_type': request.form['cookie_type'],
+        'num_of_boxes': request.form['num_of_boxes']
+    }
+    if not Order.is_valid_order(data):
+        if 'data' not in session:
+            session['data'] = data
+        return redirect('/cookies/new')
+    Order.save(data)
+    return redirect('/cookies')
+
+@app.route('/cookies/edit/<int:id>')
+def edit_order(id):
+    order = Order.get_one(id)
+    return render_template('edit.html', order=order)
--- python/flask_mysql/crud/cookie_orders/cookie_app/models/model_order.py
+++ python/flask_mysql/crud/cookie_orders/cookie_app/models/model_order.py
@@ -1,3 +1,4 @@
+from flask import flash
 from cookie_app.config.mysqlconnection import connectToMySQL
 
 
@@ -10,30 +11,29 @@ class Order:
         self.customer_name = data['customer_name']
         self.cookie_type = data['cookie_type']
         self.num_of_boxes = data['num_of_boxes']
-        self.created_at = dsata['created_at']
-        self.updated_at = data['modified_at']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
 
     @classmethod
     def get_all(cls):
-        query = 'SELECT * FROM cookie_orders'
-        results = connectToMySQL(cls.DB).query_db(quert)
+        query = 'SELECT * FROM orders'
+        results = connectToMySQL(cls.DB).query_db(query)
         orders = []
-        for order in orders:
+        for order in results:
             orders.append(cls(order))
         return orders
 
     @classmethod
     def save(cls, data):
-        if not valid_order(data):
-            pass
-        query = 'INSERT INTO cookie_orders \
+        query = 'INSERT INTO orders \
                 (customer_name, cookie_type, num_of_boxes) \
                 VALUES (%(customer_name)s, %(cookie_type)s, %(num_of_boxes)s)'
         return connectToMySQL(cls.DB).query_db(query, data)
 
     @classmethod
     def get_one(cls, data):
-        query = 'SELECT * FROM cookie_orders WHERE id = %(id)s'
+        query = 'SELECT * FROM orders WHERE id = %(id)s'
+        data = {'id': data}
         results = connectToMySQL(cls.DB).query_db(query, data)
         return cls(results[0])
 
@@ -42,5 +42,23 @@ class Order:
         query = 'SET customer_name = %(customer_name)s, \
                 cookie_type = %(cookie_type)s \
                 num_of_boxes = %(num_of_boxes)s \
-                WHERE id = %(ud)s;'
+                WHERE id = %(id)s;'
         return connectToMySQL(cls.DB).query_db(query, data)
+
+    @staticmethod
+    def is_valid_order(data):
+        is_valid = True
+        if data['customer_name'] == "" or \
+           data['cookie_type'] == "" or \
+                len(data['num_of_boxes']) < 1:
+            flash('All fields required')
+            is_valid = False
+        if len(data['customer_name']) < 2:
+            flash('Name must be at least 2 characters')
+            is_valid = False
+        if len(data['cookie_type']) < 2:
+            flash('Cookie type must be at least 2 characters')
+            is_valid = False
+        print(is_valid)
+        return is_valid
+
