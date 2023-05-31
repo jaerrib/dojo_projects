6b4bd64dc9134eec3851dd35f62fdae8dac3f553
jaerrib <jaerrib@tutanota.com>
2023-05-31 12:54:35 -0400

    Add registration/login functionality

--- python/flask_mysql/validation/login_and_registration/app/controllers/controller_users.py
+++ python/flask_mysql/validation/login_and_registration/app/controllers/controller_users.py
@@ -1,9 +1,58 @@
 from app import app
 from flask import render_template, redirect, request, session, flash
-from app.models.model_user import user
+from app.models.model_user import User
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/users/register', methods=['POST'])
+def register():
+    if not User.validate_registration(request.form):
+        return redirect('/')
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email'],
+        'password': pw_hash
+    }
+    new_user = User.save(data)
+    if not 'user' in session:
+        sesssion['user'] = new_user
+    return render_template('success.html')
+
+
+@app.route('/users/login', methods=['POST'])
+def login():
+    if not User.validate_login(request.form):
+        print('nope')
+        return redirect('/')
+    data = {
+        'email': request.form['email'],
+        'password': request.form['password']
+    }
+    user_in_db = User.get_by_email(data)
+    if not user_in_db:
+        flash('Invalid email', 'login')
+        return redirect('/')
+    if not bcrypt.check_password_hash(user_in_db.password, data['password']):
+        flash('Invalid password', 'login')
+        return rediect('/')
+    session['user'] = user_in_db
+    return render_template('success.html')
+
+
+@app.route('/users/success')
+def success():
+    return render_template('success.html')
+
+
+@app.route('/users/logout')
+def logout():
+    session.clear()
+    return redirect('/')
--- python/flask_mysql/validation/login_and_registration/app/models/model_user.py
+++ python/flask_mysql/validation/login_and_registration/app/models/model_user.py
@@ -12,8 +12,48 @@ class User:
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
+        self.email = data['email']
         self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
-    
+    @classmethod
+    def get_by_email(cls, data):
+        query = 'SELECT * FROM users WHERE email = %(email);'
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+
+    @classmethod
+    def save(cls, data):
+        query = 'INSERT INTO users (first_name, last_name, email, password) \
+            VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s;)'
+        return connectToMySQL(cls.DB).query_db(query, data)
+
+    @classmethod
+    def validate_login(cls, data):
+        is_valid = True
+        for key in data:
+            if data['key'] == "":
+                print(data['key'])
+                flash('All fields required', 'login')
+                is_valid = False
+        return is_valid
+
+    @classmethod
+    def validate_registration(cls, data):
+        is_valid = True
+        for key in data:
+            if data['key'] == "":
+                flash('All fields required', 'register')
+                is_valid = False
+        if not EMAIL_REGEX.match(data['email']):
+            flash('Invalid email address', 'register')
+            is_valid = False
+        query = 'SELECT * FROM users WHERE email = %(email)s;'
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        if len(results) != 0:
+            flash('This email already exists')
+            is_valid = False
+        return is_valid
