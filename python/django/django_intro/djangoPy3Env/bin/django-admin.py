1d9d6d99a37e5f06c9b2f112a00e7eb8b58b9ccf
jaerrib <jaerrib@tutanota.com>
2023-06-23 13:01:10 -0400

    Initial setup for django assignments

---
+++ python/django/django_intro/djangoPy3Env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/var/home/jaerrib/repositories/dojo_projects/python/django/django_intro/djangoPy3Env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
