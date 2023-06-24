69f959afd58b33c31a61775236ba8d36857aea15
jaerrib <jaerrib@tutanota.com>
2023-06-24 07:59:55 -0400

    Add more examples

--- python/django/django_intro/my_project/some_app/urls.py
+++ python/django/django_intro/my_project/some_app/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path     
 from . import views
+
 urlpatterns = [
-    path('', views.index),	   
+    path('', views.index),
+    path('another_route', views.another_method),
+    path('redirected_route', views.redirected_method),
+    path('bears', views.one_method),                        # would only match localhost:8000/bears
+    path('bears/<int:my_val>', views.another_method),       # would match localhost:8000/bears/23
+    path('bears/<str:name>/poke', views.yet_another),       # would match localhost:8000/bears/pooh/poke
+    path('<int:id>/<str:color>', views.one_more),           # would match localhost:8000/17/brown
 ]
--- python/django/django_intro/my_project/some_app/views.py
+++ python/django/django_intro/my_project/some_app/views.py
@@ -1,4 +1,27 @@
 # Create your views here.
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.http import JsonResponse
+
 def index(request):
     return HttpResponse("this is the equivalent of @app.route('/')!")
+
+def one_method(request):                # no values passed via URL
+    pass                                
+    
+def another_method(request, my_val):	# my_val would be a number from the URL
+    pass                                # given the example above, my_val would be 23
+    
+def yet_another(request, name):	        # name would be a string from the URL
+    pass                                # given the example above, name would be 'pooh'
+    
+def one_more(request, id, color):   	# id would be a number, and color a string from the URL
+    pass                                # given the example above, id would be 17 and color would be 'brown'
+
+def root_method(request):
+    return HttpResponse("String response from root_method")
+
+def another_method(request):
+    return redirect("/redirected_route")
+
+def redirected_method(request):
+    return JsonResponse({"response": "JSON response from redirected_method", "status": True})
