d2ad7183a727e5cd8245a3feb8a4d51bd576020a
jaerrib <jaerrib@tutanota.com>
2023-06-27 19:49:33 -0400

    Remove pasted code in views

--- python/django/django_intro/dojo_survey/app_one/views.py
+++ python/django/django_intro/dojo_survey/app_one/views.py
@@ -5,9 +5,6 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, "index.html")
-    # if request.method == "POST":
-    #     val_from_field_one = request.POST["one"]
-    #     val_from_field_two = request.POST["two"]
 
 
 def result(request):
