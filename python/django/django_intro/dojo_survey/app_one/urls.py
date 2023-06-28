cc120f44bbfe4cac37553f04cd7f18364735d749
jaerrib <jaerrib@tutanota.com>
2023-06-28 12:25:25 -0400

    Refactor Dojo Survey with named routes

--- python/django/django_intro/dojo_survey/app_one/urls.py
+++ python/django/django_intro/dojo_survey/app_one/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'survey'
+
 urlpatterns = [
-    path('', views.index),
-    path('result', views.result)
+    path('', views.index, name='my_index'),
+    path('result', views.result, name='my_result')
 ]
