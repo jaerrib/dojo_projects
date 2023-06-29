030412b30e3798a42f4d06f239f89cc1f4f43348
jaerrib <jaerrib@tutanota.com>
2023-06-29 19:58:37 -0400

    Finish Django CR Users assignment

--- python/django/django_orm/cr_users/users/models.py
+++ python/django/django_orm/cr_users/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- python/django/django_orm/cr_users/users/views.py
+++ python/django/django_orm/cr_users/users/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render, redirect
+from users.models import User
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "index.html")
+    context = {
+        "all_users": User.objects.all()
+    }
+    return render(request, "index.html", context)
+
+
+def process(request):
+    new_user = User(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email=request.POST['email'],
+        age=request.POST['age'],
+    )
+    new_user.save()
+    return redirect('/')
